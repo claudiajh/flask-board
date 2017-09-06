383717ab53267a949983a54456a48a9495b94fa5
claudiajh <claudia.with.jh@gmail.com>
2017-09-07 00:54:23 +0900

    add new contents

--- app/route/board.py
+++ app/route/board.py
@@ -9,3 +9,20 @@ def board_index():
     content_list = BoardModel.query.all()
 
     return render_template('show_board.html', contents=content_list)
+
+
+@app.route('/board_new', methods=['GET', 'POST'])
+def new_content():
+    if request.method == 'POST':
+        nowTime = datetime.utcnow()
+        newContents = BoardModel(title=request.form['title'],
+                                 content=request.form['content'],
+                                 user=request.form['user'],
+                                 create_time=nowTime,
+                                 modify_time=nowTime)
+
+        db.session.add(newContents)
+        db.session.commit()
+        return redirect(url_for('board_index'))
+
+    return render_template('new_content.html')
