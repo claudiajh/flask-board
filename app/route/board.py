b3f07338a82747c220ec31c5199f05b09fd0e2a8
claudiajh <claudia.with.jh@gmail.com>
2017-09-07 02:33:22 +0900

    modify content 기능 추가

--- app/route/board.py
+++ app/route/board.py
@@ -33,3 +33,22 @@ def show_content(content_no):
     showContent = BoardModel.query.filter_by(no=content_no).one()
 
     return render_template('show_content.html', show_content=showContent)
+
+
+@app.route('/board_modify/<content_no>', methods=['GET', 'POST'])
+def modify_content(content_no):
+    modifyContent = BoardModel.query.filter_by(no=content_no).one()
+
+    if request.method == 'POST':
+        nowTime = datetime.utcnow()
+
+        modifyContent.user = request.form['user']
+        modifyContent.title = request.form['title']
+        modifyContent.content = request.form['content']
+        modifyContent.modify_time = nowTime
+
+        db.session.commit()
+
+        return redirect(url_for('board_index', content_no=modifyContent.no))
+
+    return render_template('modify_content.html', modify_content=modifyContent)
