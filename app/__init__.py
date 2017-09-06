72203ed87ec45ce78518212bdcb2b59b44a2b6a9
claudiajh <claudia.with.jh@gmail.com>
2017-09-07 00:52:50 +0900

    board init commit

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:infinite@localhost/flask_board'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+from app.models import *
+from app.route import *
+
+db.create_all()
---
+++ app/models/boards.py
@@ -0,0 +1,13 @@
+from app import db
+
+
+class BoardModel(db.Model):
+    __tablename__ = 'board'
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+    no = db.Column(db.Integer, primary_key=True)  # 번호
+    title = db.Column(db.String(80), nullable=False)  # 제목
+    content = db.Column(db.String(200), nullable=False)  # 내용
+    user = db.Column(db.String(50), nullable=False)  # 작성자
+    create_time = db.Column(db.DATETIME)  # 작성일
+    modify_time = db.Column(db.DATETIME)  # 수정일
