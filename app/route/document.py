a5c560883c94fee0b3179418097d26ad66009534
claudiajh <claudia.with.jh@gmail.com>
2017-09-09 12:30:49 +0900

    no -> id, modify -> update variable name change & typo update

--- app/route/document.py
+++ app/route/document.py
@@ -20,8 +20,8 @@ def doc_add():
                                     content=request.form['content'],
                                     username=username,
                                     create_time=now,
-                                    modify_time=now,
-                                    coment_count=0)
+                                    update_time=now,
+                                    comment_count=0)
 
         db.session.add(new_content)
         db.session.commit()
@@ -30,16 +30,16 @@ def doc_add():
     return render_template('document/add.html')
 
 
-@app.route('/document/view/<content_no>')
-def doc_view(content_no):
-    view_content = DocumentModel.query.filter_by(no=content_no).one()
+@app.route('/document/view/<doc_id>')
+def doc_view(doc_id):
+    view_content = DocumentModel.query.filter_by(id=doc_id).one()
 
     return render_template('document/view.html', doc_view=view_content)
 
 
-@app.route('/document/update/<content_no>', methods=['GET', 'POST'])
-def doc_update(content_no):
-    update_content = DocumentModel.query.filter_by(no=content_no).one()
+@app.route('/document/update/<doc_id>', methods=['GET', 'POST'])
+def doc_update(doc_id):
+    update_content = DocumentModel.query.filter_by(id=doc_id).one()
 
     if request.method == 'POST':
         now = datetime.utcnow()
@@ -47,18 +47,18 @@ def doc_update(content_no):
         update_content.username = request.form['username']
         update_content.title = request.form['title']
         update_content.content = request.form['content']
-        update_content.modify_time = now
+        update_content.update_time = now
 
         db.session.commit()
 
-        return redirect(url_for('doc_index', content_no=update_content.no))
+        return redirect(url_for('doc_index', doc_id=update_content.id))
 
     return render_template('document/update.html', doc_update=update_content)
 
 
-@app.route('/document/delete/<content_no>')
-def doc_delete(content_no):
-    delete_content = DocumentModel.query.filter_by(no=content_no).one()
+@app.route('/document/delete/<doc_id>')
+def doc_delete(doc_id):
+    delete_content = DocumentModel.query.filter_by(id=doc_id).one()
 
     db.session.delete(delete_content)
     db.session.commit()
